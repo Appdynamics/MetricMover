e2c09b605e513e24f17fccf467be4d3971f72ea1
virender <virendersingh7@gmail.com>
2016-11-30 17:19:02 -0800

    first cut

--- bin/MetricMover.sh
+++ bin/MetricMover.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo $*
-java -Dmetric.populator.home=../ -cp "../lib/*:../conf" com.appdynamics.tools.configgenerator.Main $*
+java -Dmetric.populator.home=../ -cp "../lib/*:../conf" com.appdynamics.tools.metricmover.Runner $*
